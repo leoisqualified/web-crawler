3ca7e121a5fc723d5bd438f68c8366dd94b87fc7
leoisqualified <eaadenutsi@st.ug.edu.gh>
2025-03-13 00:28:55 +0000

    added export to excel functionality

--- api_scraper/remoteok_scraper.py
+++ api_scraper/remoteok_scraper.py
@@ -10,7 +10,8 @@ from email.mime.text import MIMEText
 from email.utils import COMMASPACE, formatdate
 
 BASE_URL = 'https://remoteok.com/api'
-USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36'
+USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 ' \
+'(KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36'
 REQUEST_HEADER = {
     'User-Agent': USER_AGENT,
     'Accept-Language': 'en-US, en;g = 0.5'
@@ -19,5 +20,18 @@ def get_job_postings():
     res = requests.get(url=BASE_URL, headers = REQUEST_HEADER)
     return res.json()
 
+def save_job_to_xlxs(data):
+    wb = Workbook()
+    sheet = wb.add_sheet('Jobs')
+    headers = list(data[0].keys())
+    for i, header in enumerate(headers):
+        sheet.write(0, i, header)
+    for i, job in enumerate(data[1:]):
+        for j, value in enumerate(job.values()):
+            sheet.write(i+1, j, value)
+    wb.save('remote_jobs.xlsx')
+
+
 if __name__ == '__main__':
-    print(get_job_postings()[1])
+    json = get_job_postings()[1:]
+    save_job_to_xlxs(json)
